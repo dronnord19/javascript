a55cb45d8fd1a21b326589ff2c1330e4579080df
dronnord19 <147618839+dronnord19@users.noreply.github.com>
2025-12-24 17:35:55 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 4566
+let accountEmail = "email@gmail.com"
+var accountName = "dronwashere"
+ accountCity = "jaipur"
+let accountState ;
+console.table({accountEmail, accountId,accountName, accountCity, accountState}) 
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,37 @@
+"use strict"; // treat all JS code as newer version, nowdays by default everything works in the newest state
+
+// alert( 3 + 3) // we are using nodeJS not browser hence error
+
+//generally avoid semicolons in react
+
+console.log(3+2); 
+ 
+console.log("dron") //code readeability should be high
+
+//tc39.es ecmascript is the orignal documentation for the standard defination
+
+
+let name = "dron"
+let age = 19
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+//bigint => for ver large numbers lke in stocks
+//string => ""
+//boolean => true/false
+//null => it is a standalone value
+//undefined => abhi value assign nahi hui hai
+//symbol => uniqueness
+
+
+
+// object =>
+
+
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
+
+
+// notr : typeof null = object
